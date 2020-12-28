0014dadf6d914176e31a0e3cf39d04486892c5d1
Aastha Gupta <18bit102@ietdavv.edu.in>
2020-12-28 15:09:34 +0530

    add time statistics

--- quiz.py
+++ quiz.py
@@ -1,5 +1,8 @@
 from questions_with_answers import questions
 
+import time
+start_time = time.time()
+
 def run_test(questions):
     score = 0
     for question in questions:
@@ -7,5 +10,8 @@ def run_test(questions):
         if(answer==question.answer):
             score+=1
     print("You got " + str(score)+" out of " + str(len(questions)) + " right")
+    time_taken = round(time.time() - start_time,2)
+    print("Time taken to attempt the quiz: " + str(time_taken) + " seconds")
+    print("Time taken to answer one question: "+ str(round(time_taken/len(questions),2)) + " seconds")
 
 run_test(questions)
